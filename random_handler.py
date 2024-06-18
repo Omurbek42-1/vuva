d88d1ee96f1da29537798a2d0197fa8f96b29efa
omurbek <omurbekzaanbaeva@gmail.com>
2024-06-18 23:04:02 +0600

    Delete random_handler.py

--- random_handler.py
+++
@@ -1,6 +0,0 @@
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from models.menu import get_random_dish
-
-def random_dish(update, context):
-    dish = get_random_dish()
-    update.message.reply_photo(dish.photo, caption=dish.name)
